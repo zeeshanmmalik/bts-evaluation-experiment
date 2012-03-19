c693e10f087e62537e93cec63d87372f1634f4c6
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-03-18 19:53:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BtsEvalExpApp::Application.config.secret_token = 'def874365e69628224aa37d7f5e85c296b6d0fb293028be20ddcfb974d5ffeb125ca99268c3b2545fe4c0ad1f818a2a50e7e36206c7983547d05aaaddca396f7'
