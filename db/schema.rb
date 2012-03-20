fe5f0a1d9ec01b5099d494990882f1deec022412
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-03-20 14:33:35 -0500

    migrations run

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(:version => 20120320170349) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
+    t.integer  "year",       :limit => 5
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
