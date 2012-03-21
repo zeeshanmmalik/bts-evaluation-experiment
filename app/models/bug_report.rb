c32e476fdce6aad91ed570777198c672e245f001
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-03-21 07:33:51 -0500

    associations added

--- app/models/bug_report.rb
+++ app/models/bug_report.rb
@@ -1,5 +1,5 @@
 class BugReport < ActiveRecord::Base
   belongs_to :experiment
-  belongs_to :participant #is assigned to a participant for evaluation
+  belongs_to :participant
   has_many :comments, :dependent => :destroy
 end
