e622dcb84e6014a72f9f98c07fe8fef1ad83e3a2
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-04-12 00:43:49 -0500

    export results for each and all experiments

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class ExperimentsController < ApplicationController
   before_filter :authenticate_admin!
   # GET /experiments
@@ -97,7 +98,61 @@ class ExperimentsController < ApplicationController
     end
   end
 
+  def export_results
+    experiment = Experiment.find(params[:id])
+
+    csv_string = CSV.generate do |csv|
+      cols = ["experiment","user_id","summary","question","choice"]
+      csv << cols
+      csv = experiment_csv(experiment, csv)
+    end
+    filename = "#{experiment.title}-data-#{Time.now.to_date.to_s}.csv"
+    send_data(csv_string, :type => 'text/csv; charset=utf-8; header=present', :filename => filename)  
+  end
+
+  def export_all_results
+    csv_string = CSV.generate do |csv|
+      cols = ["experiment","user_id","summary","question","choice"]
+      csv << cols
+      Experiment.all.each do |experiment|
+        csv = experiment_csv(experiment, csv)        
+      end
+    end
+    filename = "all-experiments-data-#{Time.now.to_date.to_s}.csv"
+    send_data(csv_string, :type => 'text/csv; charset=utf-8; header=present', :filename => filename)  
+  end
+
   private
+
+  def experiment_csv experiment, csv
+    total_submissions = experiment.participants.where('eval_submitted_at IS NOT NULL and eval_submitted_at != ""')
+    lex_submissions = total_submissions.where(:summary_assigned => APP_CONFIG[:lexrank])
+    email_submissions = total_submissions.where(:summary_assigned => APP_CONFIG[:email])
+    csv = submission_csv(experiment.title, lex_submissions, 'lex', csv)
+    csv = submission_csv(experiment.title, email_submissions, 'email', csv)
+    return csv
+  end
+
+  def submission_csv exp_title, submissions, str, csv
+    submissions.each do |p|
+      r = p.response
+      rep_part = [exp_title, p.id, p.summary_assigned]
+      csv << rep_part + ['1a', r.send("imp_points_incl_#{str}_sum")]
+      csv << rep_part + ['1b', r.send("redundancy_#{str}_sum")]
+      csv << rep_part + ['1c', r.send("unnecessary_info_#{str}_sum")]
+      csv << rep_part + ['1d', r.send("coherence_#{str}_sum")]
+      csv << rep_part + ['4a', r.sum_help_bug_similar]
+      csv << rep_part + ['4b', r.sum_help_bug_workaround]
+      csv << rep_part + ['4c', r.sum_help_bug_status]
+      csv << rep_part + ['4d', r.sum_help_bug_life]
+      csv << rep_part + ['4e', r.sum_help_proj_cont]
+      csv << rep_part + ['4f', r.sum_help_dev]
+      csv << rep_part + ['4g', r.sum_help_non_dev]
+      csv << rep_part + ['5', r.summary_view_pref]
+    end
+    return csv
+  end
+
   def populate_result submissions, str
     result = {:imp_points => {:r0 => 0, :r1 => 0, :r2 => 0, :r3 => 0, :r4 => 0},
       :redundancy => {:r0 => 0, :r1 => 0, :r2 => 0, :r3 => 0, :r4 => 0},
